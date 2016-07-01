018793039461334e0bcfa5f5854777717fb7df90
Saul <necrosaromx@users.noreply.github.com>
2016-07-01 16:42:59 -0500

    version con 2 imagenes
    ya pudimos poner al slime!

--- src/main.c
+++ src/main.c
@@ -7,17 +7,19 @@ static GFont s_time_font;
 static GFont s_date_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
+static BitmapLayer *s_slime_layer;
+static GBitmap *s_slime_bitmap;
 
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-  static char s_buffer_date[8];
+  static char s_buffer_date[12];
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
-  strftime(s_buffer_date, sizeof(s_buffer_date), "%D", tick_time);
+  strftime(s_buffer_date, sizeof(s_buffer_date), "%b %e!", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
@@ -32,8 +34,9 @@ static void main_window_load(Window *window) {
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  //GRect slime_frame = (0, 120, 33, 33);
   // Create GBitmap
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DW_BG);
 
   // Create BitmapLayer to display the GBitmap
   s_background_layer = bitmap_layer_create(bounds);
@@ -42,16 +45,21 @@ static void main_window_load(Window *window) {
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
+      // Create SLIME
+  s_slime_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SLIME);
+  s_slime_layer = bitmap_layer_create(GRect(73, 120, 33, 33));
+  bitmap_layer_set_bitmap(s_slime_layer, s_slime_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_slime_layer));  
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+      GRect(3, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
   s_date_layer = text_layer_create(
-      GRect(0, 0, bounds.size.w, 50));
+      GRect(0, 0, bounds.size.w, bounds.size.h));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_48));
@@ -61,15 +69,15 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_date_layer, GColorBlack);
+  text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_text(s_date_layer, "HALLO");
   // Create GFont
-  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DW_FONT_24));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_DW_FONT_48));
 
   // Apply to TextLayer
   text_layer_set_font(s_date_layer, s_date_font);
-  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentLeft);
 
   
   // Add it as a child layer to the Window's root layer
@@ -87,10 +95,12 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_time_layer);
     // Destroy GBitmap
-gbitmap_destroy(s_background_bitmap);
+  gbitmap_destroy(s_slime_bitmap);
+  bitmap_layer_destroy(s_slime_layer);
+  gbitmap_destroy(s_background_bitmap);
 
 // Destroy BitmapLayer
-bitmap_layer_destroy(s_background_layer);
+  bitmap_layer_destroy(s_background_layer);
 }
 
 
