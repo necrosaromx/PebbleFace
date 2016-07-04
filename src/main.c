612d3f488f4141b560dd28dcc6ab4242d686bc15
Saul <necrosaromx@users.noreply.github.com>
2016-07-04 18:09:44 -0500

    Slime en posición

--- src/main.c
+++ src/main.c
@@ -14,12 +14,12 @@ static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-  static char s_buffer_date[20];
+  static char s_buffer_date[8];
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
-  strftime(s_buffer_date, sizeof(s_buffer_date), "%A!%e %B", tick_time);
+  strftime(s_buffer_date, sizeof(s_buffer_date), "%a!%e", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
@@ -39,31 +39,31 @@ static void main_window_load(Window *window) {
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DW_BG);
 
   // Create BitmapLayer to display the GBitmap
-  s_background_layer = bitmap_layer_create(bounds);
+  s_background_layer = bitmap_layer_create(GRect(bounds.origin.x, bounds.origin.y + 52, bounds.size.w, bounds.size.h));
 
   // Set the bitmap onto the layer and add to the window
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
       // Create SLIME
-  GRect slime_bounds = gbitmap_get_bounds(s_slime_bitmap);
+  //GRect slime_bounds = gbitmap_get_bounds(s_slime_bitmap);
   s_slime_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SLIME);
-  s_slime_layer = bitmap_layer_create(GRect((bounds.size.w / 2) - ((slime_bounds.size.w / 2)), 120, 33, 33));
+  s_slime_layer = bitmap_layer_create(GRect((bounds.size.w / 2) - 16, 58, 33, 33));
   bitmap_layer_set_bitmap(s_slime_layer, s_slime_bitmap);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_slime_layer));  
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(3, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+      GRect(3, 120, bounds.size.w, 50));
   s_date_layer = text_layer_create(
-      GRect(0, -35, bounds.size.w, bounds.size.h));
+      GRect(9, 79, bounds.size.w, bounds.size.h));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text(s_time_layer, "00:00");
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_48));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PERFECT_DOS_36));
 
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
